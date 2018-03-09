d1baa76827118fd055c523716348310b1ae89a2b
Juan Manuel Berros <juanmaberros@gmail.com>
2018-03-09 18:32:49 -0300

    removed depracted ordering in pandas

--- vcf_to_dataframe/helpers.py
+++ vcf_to_dataframe/helpers.py
@@ -1,3 +1,4 @@
+from pandas.api.types import CategoricalDtype
 import numpy as np
 
 
@@ -7,15 +8,20 @@ def make_chromosome_series_categorical(series):
     Series with the same values.
     """
     new_series = series.astype(str)  # Creates a new copy of the series
+
     if all(new_series.str.startswith('chr')):
         new_series = new_series.str.replace('chr', '')
 
-    new_series = new_series.replace('23', 'X').replace('24', 'Y').astype(str)
-    chromosomes = [str(chrom) for chrom in list(range(1, 23)) + ['X', 'Y']
-                   if str(chrom) in new_series.values]
-    new_series = new_series.astype('category', categories=chromosomes,
-                                   ordered=True)
-    return new_series
+    sorted_chromosomes = [str(chrom)
+                          for chrom in list(range(1, 23)) + ['X', 'Y']
+                          if str(chrom) in new_series.values]
+    chromosome_dtype = CategoricalDtype(categories=sorted_chromosomes,
+                                        ordered=True)
+
+    return (new_series
+            .replace('23', 'X')
+            .replace('24', 'Y')
+            .astype(chromosome_dtype))
 
 
 def nan_to_None(value):
